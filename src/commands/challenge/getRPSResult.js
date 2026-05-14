9e30847e0a2c5682779ac565a510d5b43e580887
atb2903 <aburnham2903@gmail.com>
2026-05-14 12:11:55 -0700

    refactor getRPSResult into dedicated file

--- src/commands/challenge/getRPSResult.js
+++ src/commands/challenge/getRPSResult.js
@@ -1,24 +1,24 @@
 import { formatRPSResult } from './formatRPSResult.js';
-import { RPSChoices } from './rpsGame.js';
+import { rpsChoices } from './rpsChoices.js';
 
 export function getRPSResult(p1, p2) {
   let gameResult;
-  if (RPSChoices[p1.objectName] && RPSChoices[p1.objectName][p2.objectName]) {
+  if (rpsChoices[p1.objectName] && rpsChoices[p1.objectName][p2.objectName]) {
     // o1 wins
     gameResult = {
       win: p1,
       lose: p2,
-      verb: RPSChoices[p1.objectName][p2.objectName],
+      verb: rpsChoices[p1.objectName][p2.objectName],
     };
   } else if (
-    RPSChoices[p2.objectName] &&
-    RPSChoices[p2.objectName][p1.objectName]
+    rpsChoices[p2.objectName] &&
+    rpsChoices[p2.objectName][p1.objectName]
   ) {
     // o2 wins
     gameResult = {
       win: p2,
       lose: p1,
-      verb: RPSChoices[p2.objectName][p1.objectName],
+      verb: rpsChoices[p2.objectName][p1.objectName],
     };
   } else {
     // tie -- win/lose don't
--- src/commands/challenge/rpsGame.js
+++ src/commands/challenge/rpsGame.js
@@ -1,60 +1,11 @@
 import { capitalize } from '../../utils/capitalize.js';
-
-export function formatRPSResult(result) {
-  const { win, lose, verb } = result;
-  return verb === 'tie'
-    ? `<@${win.id}> and <@${lose.id}> draw with **${win.objectName}**`
-    : `<@${win.id}>'s **${win.objectName}** ${verb} <@${lose.id}>'s **${lose.objectName}**`;
-}
+import { rpsChoices } from './rpsChoices.js';
 
 // this is just to figure out winner + verb
-export const RPSChoices = {
-  rock: {
-    description: 'sedimentary, igneous, or perhaps even metamorphic',
-    virus: 'outwaits',
-    computer: 'smashes',
-    scissors: 'crushes',
-  },
-  cowboy: {
-    description: 'yeehaw~',
-    scissors: 'puts away',
-    wumpus: 'lassos',
-    rock: 'steel-toe kicks',
-  },
-  scissors: {
-    description: 'careful ! sharp ! edges !!',
-    paper: 'cuts',
-    computer: 'cuts cord of',
-    virus: 'cuts DNA of',
-  },
-  virus: {
-    description: 'genetic mutation, malware, or something inbetween',
-    cowboy: 'infects',
-    computer: 'corrupts',
-    wumpus: 'infects',
-  },
-  computer: {
-    description: 'beep boop beep bzzrrhggggg',
-    cowboy: 'overwhelms',
-    paper: 'uninstalls firmware for',
-    wumpus: 'deletes assets for',
-  },
-  wumpus: {
-    description: 'the purple Discord fella',
-    paper: 'draws picture on',
-    rock: 'paints cute face on',
-    scissors: 'admires own reflection in',
-  },
-  paper: {
-    description: 'versatile and iconic',
-    virus: 'ignores',
-    cowboy: 'gives papercut to',
-    rock: 'covers',
-  },
-};
+
 
 export function getRPSChoices() {
-  return Object.keys(RPSChoices);
+  return Object.keys(rpsChoices);
 }
 
 // Function to fetch shuffled options for select menu
@@ -68,7 +19,7 @@ export function getShuffledRPSOptions() {
     options.push({
       label: capitalize(c),
       value: c.toLowerCase(),
-      description: RPSChoices[c]['description'],
+      description: rpsChoices[c]['description'],
     });
   }
 
