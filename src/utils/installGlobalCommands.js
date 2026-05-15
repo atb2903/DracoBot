f8ab80441ff2ccc2655c1b2be716d144033e2a6b
atb2903 <aburnham2903@gmail.com>
2026-05-15 13:33:59 -0700

    document installGlobalCommands

--- src/utils/installGlobalCommands.js
+++ src/utils/installGlobalCommands.js
@@ -1,5 +1,12 @@
 import { discordRequest } from './discordRequest.js';
 
+/**
+ * @memberof module:utils
+ * @function installGlobalCommands
+ * @description Installs or updates global application commands for a Discord bot using the Discord API.
+ * @param {string} appId - The application ID of the Discord bot.
+ * @param {Array} commands - An array of command objects to be registered as global commands.
+ */
 export async function installGlobalCommands(appId, commands) {
   // API endpoint to overwrite global commands
   const endpoint = `applications/${appId}/commands`;
