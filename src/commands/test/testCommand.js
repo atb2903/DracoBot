1acf2edf8bdc2d677de061a4f591541f72f4cf61
atb2903 <aburnham2903@gmail.com>
2026-05-15 12:52:43 -0700

    document testCommand

--- src/commands/test/testCommand.js
+++ src/commands/test/testCommand.js
@@ -5,6 +5,12 @@ import {
 } from 'discord-interactions';
 import { getRandomEmoji } from '../../utils/getRandomEmoji.js';
 
+/**
+ * @memberof module:testCommand
+ * @function testCommand
+ * @description A simple test command that responds with a greeting and a random emoji.
+ * @param {Object} res - The response object for sending interaction responses.
+ */
 export function testCommand(res) {
     // Send a message into the channel where command was triggered from
     return res.send({
