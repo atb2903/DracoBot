dd64f3030a559cdd392730cdaf40a17773ac0628
atb2903 <aburnham2903@gmail.com>
2026-05-15 12:18:11 -0700

    remove unneeded exports

--- src/commands/challenge/index.js
+++ src/commands/challenge/index.js
@@ -3,10 +3,3 @@
  * @module challengeCommand
  * @description Handles challenge initiation and acceptance logic
  */
-
-export { challengeCommand } from './challengeCommand.js';
-export { acceptButton } from './acceptButton.js';
-export { formatRPSResult } from './formatRPSResult.js';
-export { getRPSChoices } from './getRPSChoices.js';
-export { getRPSResult } from './getRPSResult.js';
-export { getShuffledRPSOptions } from './getShuffledRPSOptions.js';
