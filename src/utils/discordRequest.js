c5d3218eddb4ec444ab82e0f3c5768ccba3aa6e6
atb2903 <aburnham2903@gmail.com>
2026-05-15 13:00:39 -0700

    document discordRequest

--- src/utils/discordRequest.js
+++ src/utils/discordRequest.js
@@ -1,5 +1,14 @@
 import 'dotenv/config';
 
+/**
+ * @memberof module:utils
+ * @function discordRequest
+ * @description A helper function to make API requests to Discord's REST API.
+ * @param {string} endpoint - The API endpoint to send the request to (e.g., 'channels/{channel.id}/messages').
+ * @param {Object} options - The options for the fetch request, including method, body, etc.
+ * @returns {Promise<Response>} The response from the Discord API.
+ * @throws Will throw an error if the API response is not ok (status code not in the range 200-299).
+ */
 export async function discordRequest(endpoint, options) {
   // append endpoint to root API URL
   const url = 'https://discord.com/api/v10/' + endpoint;
